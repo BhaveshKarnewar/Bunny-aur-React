afd01d33dde192f5f53d1501a4e8b8623fe3e1fc
BhaveshKarnewar <bhaveshkarnewar2@gmail.com>
2024-03-11 17:24:05 +0530

    add redux toolkit

---
+++ 11reduxToolkit/src/components/Todos.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo, updateTodo } from "../features/todo/todoSlice";
+
+export default function Todos() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div>Todos</div>
+      <div className="flex flex-col justify-center items-center my-[10px]">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="w-[400px] h-[50px] bg-cyan-300 flex justify-center items-center my-[10px]"
+          >
+            <span className="text-[30px] text-amber-900">
+              {todo.text.toUpperCase()}
+            </span>
+            <button
+              className="px-[20px]"
+              onClick={() => {
+                dispatch(removeTodo(todo.id));
+              }}
+            >
+              del
+            </button>
+          </li>
+        ))}
+      </div>
+    </>
+  );
+}
