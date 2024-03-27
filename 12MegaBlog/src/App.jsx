1662aacb6f601d5434162634f820a4686d56a841
BhaveshKarnewar <bhaveshkarnewar2@gmail.com>
2024-03-27 23:04:40 +0530

    update megablog

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
-function App() {
-  // console.log("bhabehs");
-  // console.log(import.meta.env.VITE_APPWRITE_URL);
-  console.log(authService);
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
-  return (
-    <>
-      <h2>Blog with appwrite</h2>
-    </>
-  );
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block font-bold text-xl">
+        <Header />
+        <main>
+          TODO:
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
